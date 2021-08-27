c2c73bfd3fb8479bd9c64a372bf2ae490008a3af
Cypmon <randalchem@protonmail.com>
2021-08-27 14:59:39 -0300

    Agregada escritura con solo labels.

--- getlabels.py
+++ getlabels.py
@@ -1,11 +1,19 @@
-from pdfrw import PdfReader
+from pdfrw import PdfReader, PdfWriter
 from pagelabels import PageLabels
 
 reader = PdfReader("CMI_clase2.pdf")
 labels = PageLabels.from_pdf(reader)
-pageAmount = len(reader.pages)
 
-pages = [i.startpage for i in labels]
-pages.append(pageAmount)
-print(pages)
+pages = reader.pages
+pageAmount = len(pages)
+
+pageLabels = [i.startpage for i in labels][1:]
+pageLabels.append(pageAmount)
+print(pageLabels)
+
+outdata = PdfWriter("out.pdf")
+for p in pageLabels:
+    outdata.addpage(pages[p-1])
+
+outdata.write()
 
